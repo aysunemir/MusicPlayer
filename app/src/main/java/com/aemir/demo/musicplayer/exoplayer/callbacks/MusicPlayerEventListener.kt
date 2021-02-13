3500c31207e4ec6756596a01c60c4568b911d203
Aysun Emir <aysun.emir@gmail.com>
2021-02-13 15:52:23 +0300

    preparing the music player

---
+++ app/src/main/java/com/aemir/demo/musicplayer/exoplayer/callbacks/MusicPlayerEventListener.kt
@@ -0,0 +1,24 @@
+package com.aemir.demo.musicplayer.exoplayer.callbacks
+
+import android.widget.Toast
+import com.aemir.demo.musicplayer.exoplayer.MusicService
+import com.google.android.exoplayer2.ExoPlaybackException
+import com.google.android.exoplayer2.Player
+
+class MusicPlayerEventListener(
+    private val musicService: MusicService
+) : Player.EventListener {
+
+    override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+        super.onPlayerStateChanged(playWhenReady, playbackState)
+        if (playbackState == Player.STATE_READY && !playWhenReady) {
+            musicService.stopForeground(false)
+        }
+    }
+
+    override fun onPlayerError(error: ExoPlaybackException) {
+        super.onPlayerError(error)
+        Toast.makeText(musicService, "An unknown error occurred", Toast.LENGTH_LONG).show()
+    }
+
+}
