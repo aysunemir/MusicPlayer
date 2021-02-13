accefacbe7979ce11c38a23ab94fedbdc271ef4f
Aysun Emir <aysun.emir@gmail.com>
2021-02-13 15:20:25 +0300

    setting up the music notification

--- app/src/main/java/com/aemir/demo/musicplayer/other/Constants.kt
+++ app/src/main/java/com/aemir/demo/musicplayer/other/Constants.kt
@@ -3,4 +3,7 @@ package com.aemir.demo.musicplayer.other
 object Constants {
 
     const val SONG_COLLECTION = "songs"
+
+    const val NOTIFICATION_CHANNEL_ID = "music"
+    const val NOTIFICATION_ID = 1
 }
