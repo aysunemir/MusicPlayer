c1a690341067bf50d847186ff81f6c958d496c81
Aysun Emir <aysun.emir@gmail.com>
2021-02-14 19:11:18 +0300

    swiping through songs

--- app/src/main/java/com/aemir/demo/musicplayer/ui/MainActivity.kt
+++ app/src/main/java/com/aemir/demo/musicplayer/ui/MainActivity.kt
@@ -1,14 +1,73 @@
 package com.aemir.demo.musicplayer.ui
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.aemir.demo.musicplayer.R
+import com.aemir.demo.musicplayer.adapters.SwipeSongAdapter
+import com.aemir.demo.musicplayer.data.entities.Song
+import com.aemir.demo.musicplayer.databinding.ActivityMainBinding
+import com.aemir.demo.musicplayer.exoplayer.toSong
+import com.aemir.demo.musicplayer.other.Status
+import com.aemir.demo.musicplayer.ui.viewmodels.MainViewModel
+import com.bumptech.glide.RequestManager
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val mainViewModel: MainViewModel by viewModels()
+
+    @Inject
+    lateinit var swipeSongAdapter: SwipeSongAdapter
+
+    @Inject
+    lateinit var glide: RequestManager
+
+    private var curPlayingSong: Song? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        subscribeToObservers()
+        binding.vpSong.adapter = swipeSongAdapter
+    }
+
+    private fun switchViewPagerToCurrentSong(song: Song) {
+        val newItemIndex = swipeSongAdapter.currentList.indexOf(song)
+        if (newItemIndex != -1) {
+            binding.vpSong.currentItem = newItemIndex
+            curPlayingSong = song
+        }
+    }
+
+    private fun subscribeToObservers() {
+        mainViewModel.mediaItems.observe(this) {
+            it?.let { result ->
+                when (result.status) {
+                    Status.SUCCESS -> {
+                        result.data?.let { songs ->
+                            swipeSongAdapter.submitList(songs)
+                            if (songs.isNotEmpty()) {
+                                glide.load(curPlayingSong ?: songs[0].imageUrl)
+                                    .into(binding.ivCurSongImage)
+                            }
+                            switchViewPagerToCurrentSong(curPlayingSong ?: return@observe)
+                        }
+                    }
+                    Status.ERROR -> Unit
+                    Status.LOADING -> Unit
+                }
+            }
+        }
+
+        mainViewModel.curPlayingSong.observe(this) {
+            if (it == null) return@observe
+            curPlayingSong = it.toSong()
+            glide.load(curPlayingSong?.imageUrl).into(binding.ivCurSongImage)
+            switchViewPagerToCurrentSong(curPlayingSong ?: return@observe)
+        }
     }
 }
